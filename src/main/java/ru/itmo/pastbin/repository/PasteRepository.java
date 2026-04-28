7ad98fa362da1eeae5e7e9e9d65bad8525a5e6b3
y9mato <xmmdkfkfxmkdkf@gmail.com>
2026-04-28 12:41:16 +0300

    feat: add PasteRepository

--- src/main/java/ru/itmo/pastbin/repository/PasteRepository.java
+++ src/main/java/ru/itmo/pastbin/repository/PasteRepository.java
@@ -1,4 +1,17 @@
 package ru.itmo.pastbin.repository;
 
-public interface PasteRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.itmo.pastbin.entity.Paste;
+
+import java.util.Optional;
+
+/**
+ * репозиторий для работы с paste
+ */
+
+public interface PasteRepository extends JpaRepository<Paste, Long>{
+    /**
+     * найти paste по hash
+     */
+    Optional<Paste> findByHash(String hash);
 }
--- src/main/java/ru/itmo/pastbin/service/PasteService.java
+++ src/main/java/ru/itmo/pastbin/service/PasteService.java
@@ -1,6 +1,9 @@
 package ru.itmo.pastbin.service;
 
 
+import org.springframework.stereotype.Service;
+import ru.itmo.pastbin.repository.PasteRepository;
+import ru.itmo.pastbin.entity.Paste;
 /**
  * сервис для работы с paste.
  *
@@ -9,6 +12,11 @@ package ru.itmo.pastbin.service;
  * - создание paste
  * - работа с БД
  */
-
+@Service
 public class PasteService {
+    private final PasteRepository pasteRepository;
+
+    public PasteService(PasteRepository pasteRepository) {
+        this.pasteRepository = pasteRepository;
+    }
 }
