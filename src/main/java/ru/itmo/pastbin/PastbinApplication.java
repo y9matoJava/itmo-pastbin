f2521a29e9ee9c6336d5c7abec93d4a08c0007a2
y9mato <xmmdkfkfxmkdkf@gmail.com>
2026-04-28 11:38:21 +0300

    chore: initial commit

---
+++ src/main/java/ru/itmo/pastbin/PastbinApplication.java
@@ -0,0 +1,13 @@
+package ru.itmo.pastbin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastbinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PastbinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/itmo/pastbin/PastbinApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itmo.pastbin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastbinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
