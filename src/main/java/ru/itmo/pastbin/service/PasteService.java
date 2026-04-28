4dadf4fda8a7253eaf354459734b503904484d45
y9mato <xmmdkfkfxmkdkf@gmail.com>
2026-04-28 12:26:24 +0300

    feat: start PasteService

---
+++ src/main/java/ru/itmo/pastbin/service/PasteService.java
@@ -0,0 +1,14 @@
+package ru.itmo.pastbin.service;
+
+
+/**
+ * сервис для работы с paste.
+ *
+ * здесь находится вся бизнес-логика:
+ * - генерация hash
+ * - создание paste
+ * - работа с БД
+ */
+
+public class PasteService {
+}
