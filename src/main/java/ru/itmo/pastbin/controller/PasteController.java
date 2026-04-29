8435d0976f4adc69ae6ed929c38704c376e320af
y9mato <xmmdkfkfxmkdkf@gmail.com>
2026-04-29 19:17:56 +0300

    feat: add temprary PasteController for testing

---
+++ src/main/java/ru/itmo/pastbin/controller/PasteController.java
@@ -0,0 +1,21 @@
+package ru.itmo.pastbin.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.itmo.pastbin.service.PasteService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/pastes")
+public class PasteController {
+    private final PasteService pasteService;
+
+    @PostMapping
+    public String createPaste(@RequestBody String text) {
+        pasteService.createPaste(text, text, 5);
+        return "бубаби";
+    }
+}
