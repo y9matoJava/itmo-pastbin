44112f6482986910c3ffad26ef485384ede16f7c
y9mato <xmmdkfkfxmkdkf@gmail.com>
2026-05-03 23:32:34 +0300

    feat(controller): add get paste endpoint by hash

--- src/main/java/ru/itmo/pastbin/controller/PasteController.java
+++ src/main/java/ru/itmo/pastbin/controller/PasteController.java
@@ -1,10 +1,8 @@
 package ru.itmo.pastbin.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.itmo.pastbin.dto.PasteResponseDto;
 import ru.itmo.pastbin.service.PasteService;
 
 @RestController
@@ -13,9 +11,20 @@ import ru.itmo.pastbin.service.PasteService;
 public class PasteController {
     private final PasteService pasteService;
 
-    @PostMapping
-    public String createPaste(@RequestBody String text) {
-        pasteService.createPaste("тест", text, 5);
-        return "бубаби";
+    /**
+     * Получение пасты по короткому хешу
+     *
+     * пример запроса: GET /api/pastes/Ab9
+     *
+     * Благодаря @Cachable и PasteService, повторные запросы
+     * к популярным постам будут отдаваться из redis кшэша
+     * не нагружая Postgres и MinIO
+     *
+     * @param hash короткий хеш из url
+     * @return json с метаданными и текстом пасты
+     */
+    @GetMapping("/{hash}")
+    public PasteResponseDto getPaste(@PathVariable String hash) {
+        return pasteService.getByHash(hash);
     }
 }
