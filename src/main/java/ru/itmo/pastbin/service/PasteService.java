06228b9505a7e9abba0ebd4ed13eac8f4f1838cf
y9mato <xmmdkfkfxmkdkf@gmail.com>
2026-05-03 23:26:57 +0300

    feat(service): add getByHash with Redis caching

--- src/main/java/ru/itmo/pastbin/service/PasteService.java
+++ src/main/java/ru/itmo/pastbin/service/PasteService.java
@@ -1,6 +1,7 @@
 package ru.itmo.pastbin.service;
 
-
+import org.springframework.cache.annotation.Cacheable;
+import ru.itmo.pastbin.dto.PasteResponseDto;
 import org.springframework.stereotype.Service;
 import ru.itmo.pastbin.repository.PasteRepository;
 import ru.itmo.pastbin.entity.Paste;
@@ -68,5 +69,51 @@ public class PasteService {
         paste.setViewsCount(0L);
 
         return pasteRepository.save(paste);
+    }
+
+    /**
+     * Получает пасту по короткому хешу.
+     *
+     * @Cacheable - спринг автоматически кэширует результат в Redis
+     *
+     * Как он работает:
+     * 1) Первый запрос с hash="Ab9" -> метод выполняется полностью
+     *  (БД + MinIO), результат сохраняется в Redis под ключом "pastes::Ab9"
+     * 2) Повторный запрос с hash="Ab9" -> метод НЕ вызывается,
+     *  результат берется напрямую из redis (время работы 0.1мс вместо 50мс)
+     *
+     * value = "pastes" имя кэша
+     * key = "#hash" по какому параметру кэшировать
+     *
+     * @param hash короткий хеш из url
+     * @return DTO с метаданными из текстов пасты
+     * @throws RuntimeException если паста не найдена или истекла
+     */
+    @Cacheable(value = "pastes", key = "#hash")
+    public PasteResponseDto getByHash(String hash) {
+        // шаг 1: ищем метаданные в PostgreSQL
+        Paste paste = pasteRepository.findByHash(hash)
+                .orElseThrow(() -> new RuntimeException("паста не найдена: " + hash));
+
+        // шаг 2: проверяем, не истекла ли паста
+        if (!paste.getActive() || paste.getExpiresAt().isBefore(LocalDateTime.now())) {
+            throw new RuntimeException("паста истекла: " + hash);
+        }
+
+        // шаг 3: загружаем текст из MinIO
+        String content = storageService.download(paste.getObjectKey());
+
+        // шаг 4: увеличиваем счетчик просмотров
+        paste.setViewsCount(paste.getViewsCount() + 1);
+        pasteRepository.save(paste);
+
+        // шаг 5: собираем DTO
+        return new PasteResponseDto(
+                paste.getHash(),
+                paste.getTitle(),
+                content,
+                paste.getCreatedAt(),
+                paste.getExpiresAt()
+        );
     }
 }
