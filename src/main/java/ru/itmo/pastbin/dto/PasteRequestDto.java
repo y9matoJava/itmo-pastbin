8f19eb98898c9d3fe9d8302ca41ebcc97902303a
y9mato <xmmdkfkfxmkdkf@gmail.com>
2026-05-04 12:49:32 +0300

    feat: add PasteRequestDto

---
+++ src/main/java/ru/itmo/pastbin/dto/PasteRequestDto.java
@@ -0,0 +1,34 @@
+package ru.itmo.pastbin.dto;
+
+/**
+ * DTO для запроса на создание пасты
+ *
+ * Клиент отправляет json:
+ * {
+ *     "title": "Мой код",
+ *     "content": "System.out.println(\"Hello\");",
+ *     "ttlMinutes": 30
+ * }
+ * Spring автоматически десериализует json в этот объект
+ */
+public class PasteRequestDto {
+    private String title;
+    private String content;
+    private int ttlMinutes = 60;
+
+    public PasteRequestDto() {}
+    public String getTitle() { return title; }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() { return content; }
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getTtlMinutes() { return ttlMinutes; }
+    public void setTtlMinutes(int ttlMinutes) {
+        this.ttlMinutes = ttlMinutes;
+    }
+}
