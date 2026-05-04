b9603a3a9b8d4914785fb5c93f7b804a2438e0d6
y9mato <xmmdkfkfxmkdkf@gmail.com>
2026-05-04 09:06:18 +0300

    feat(scheduling): enable scheduled cleanup tasks

--- src/main/java/ru/itmo/pastbin/PastbinApplication.java
+++ src/main/java/ru/itmo/pastbin/PastbinApplication.java
@@ -2,8 +2,10 @@ package ru.itmo.pastbin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class PastbinApplication {
 
     public static void main(String[] args) {
