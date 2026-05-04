170578d183bd7c11c7f4a68744574395d7c27a2b
y9mato <xmmdkfkfxmkdkf@gmail.com>
2026-05-04 09:04:10 +0300

    feat(repository): add method to find expired active pastes

--- src/main/java/ru/itmo/pastbin/repository/PasteRepository.java
+++ src/main/java/ru/itmo/pastbin/repository/PasteRepository.java
@@ -3,6 +3,8 @@ package ru.itmo.pastbin.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.itmo.pastbin.entity.Paste;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -14,4 +16,16 @@ public interface PasteRepository extends JpaRepository<Paste, Long>{
      * найти paste по hash
      */
     Optional<Paste> findByHash(String hash);
+
+    /**
+     * найти все активные пасты, у которых срок жизни истек.
+     *
+     * Spring Data JPA сам генерирует SQL по имени метода:
+     * findByACtiveTrueAndExpiresAtBefore(now) ->
+     *  SELECT * FROM pastes WHERE active = true AND expires_at < now
+     *
+     * @param now текущее время
+     * @return список просроченных паст
+     */
+    List<Paste> findByActiveTrueAndExpiresAtBefore(LocalDateTime now);
 }
