1bf0266aafedd05a63faaf2beba3d6c1a0b1990b
y9mato <xmmdkfkfxmkdkf@gmail.com>
2026-05-04 13:32:39 +0300

    fix(config): correct rate limit path pattern

--- src/main/java/ru/itmo/pastbin/config/WebConfig.java
+++ src/main/java/ru/itmo/pastbin/config/WebConfig.java
@@ -40,6 +40,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(rateLimitingInterceptor)
-                .addPathPatterns("/api/pastes/");
+                .addPathPatterns("/api/pastes");
     }
 }
